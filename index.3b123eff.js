d863d7f514cf1a2bff24e2726c9e824b9e34f4bc
Nierochron <166538574+Nierochron@users.noreply.github.com>
2025-07-25 17:34:03 +0000

    Deploying to gh-pages from @ Nierochron/Interactive-games@6684b66fbeb955725947b1200c1350fdee8b1716 🚀

---
+++ index.3b123eff.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("h1");console.log(o);
+//# sourceMappingURL=index.3b123eff.js.map
---
+++ index.5eeabdf4.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("h1");console.log(o)}();
+//# sourceMappingURL=index.5eeabdf4.js.map
