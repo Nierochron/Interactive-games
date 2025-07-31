b3e8fa0392d62adbe5a6bd7cffd1ba07c18828a8
Katya-Semeniuk <109156601+Katya-Semeniuk@users.noreply.github.com>
2025-07-31 17:27:05 +0000

    Deploying to gh-pages from @ Nierochron/Interactive-games@0ceb240265cd7d0d4963a1b0596c2b3e8933a2b8 🚀

--- index.3b123eff.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("h1");console.log(o);
-//# sourceMappingURL=index.3b123eff.js.map
--- index.5eeabdf4.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector("h1");console.log(o)}();
-//# sourceMappingURL=index.5eeabdf4.js.map
