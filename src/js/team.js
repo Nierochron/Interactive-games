685b821d2ff61e2332b4f2b6a3c3d6e938f7b262
Maxim Yuzviak <shaylushaytop@gmail.com>
2025-08-07 20:57:06 +0300

    Add team.js, have problem with img

--- src/js/team.js
+++ src/js/team.js
@@ -1,24 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
+import imgUrl1 from './images/team/ivan-logo.jpg';
+import imgUrl2 from './images/team/max-logo.png';
+import imgUrl3 from './images/team/oleg-logo.png';
+
   const team = [
     {
       name: 'Nierochron',
       info: 'Team Lid of our team. Created sections: modal window, time calculator, Google dinosaur, football, 3 numbers',
-      img: './images/team/ivan-logo.jpg',
+      img: imgUrl1,
     },
     {
       name: 'The_kotik',
       info: 'Created sections: team, scientists, footer.',
-      img: './images/team/max-logo.png',
+      img: imgUrl2,
     },
     {
       name: 'OlegMusu',
       info: 'Created sections: born year, guess number, rock paper scissors, calculator',
-      img: './images/team/oleg-logo.png',
+      img: imgUrl3,
     },
   ];
 
   const image = document.getElementById('team-photo');
-  const nextButton = document.getElementById('team-button-right');
+const nextButton = document.getElementById('team-button-right');
+  console.log(nextButton);
 
   let currentIndex = 0;
 
@@ -28,16 +32,12 @@ document.addEventListener('DOMContentLoaded', () => {
     image.alt = `Фото ${currentPhoto.name}`;
   }
 
-  nextButton.addEventListener('click', () => {
+nextButton.addEventListener('click', () => {
+    console.log("Hello World");
     currentIndex++;
     if (currentIndex >= team.length) {
       currentIndex = 0;
     }
-    
-    console.log('Завантажую картинку:', currentPhoto.img);
     firstImg();
   });
-
-  console.log('Завантажую картинку:', currentPhoto.img);
   firstImg();
-});
