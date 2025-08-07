d200faa18aeb34d76d7332b73427f979b4fd8cd4
Maxim Yuzviak <shaylushaytop@gmail.com>
2025-08-07 20:46:35 +0300

    Add team.js

--- src/js/team.js
+++ src/js/team.js
@@ -1,26 +1,43 @@
-const team = [
-  {
-    name: 'Nierochron',
-    info: 'Team Lid of our team. Created sections: modal window, time calculator, Google dinosaur, football, 3 numbers',
-    img: 'images/team/ivan-logo.jpg',
-  },
-  {
-    name: 'The_kotik',
-    info: 'Created sections: team, scientists, footer.',
-    img: 'images/team/max-logo.png',
-  },
-  {
-    name: 'OlegMusu',
-    info: 'Created sections: born year, guess number, rock paper scissors, calculator',
-    img: 'images/team/oleg-logo.png',
-  },
-];
+document.addEventListener('DOMContentLoaded', () => {
+  const team = [
+    {
+      name: 'Nierochron',
+      info: 'Team Lid of our team. Created sections: modal window, time calculator, Google dinosaur, football, 3 numbers',
+      img: './images/team/ivan-logo.jpg',
+    },
+    {
+      name: 'The_kotik',
+      info: 'Created sections: team, scientists, footer.',
+      img: './images/team/max-logo.png',
+    },
+    {
+      name: 'OlegMusu',
+      info: 'Created sections: born year, guess number, rock paper scissors, calculator',
+      img: './images/team/oleg-logo.png',
+    },
+  ];
 
+  const image = document.getElementById('team-photo');
+  const nextButton = document.getElementById('team-button-right');
 
-const photoContainer = document.getElementById('team-photo');
-team.forEach(member => {
-  const img = document.createElement('img');
-  img.src = member.img;
-  img.alt = `Фото ${member.name}`;
-  galleryContainer.appendChild(img);
+  let currentIndex = 0;
+
+  function firstImg() {
+    const currentPhoto = team[currentIndex];
+    image.src = currentPhoto.img;
+    image.alt = `Фото ${currentPhoto.name}`;
+  }
+
+  nextButton.addEventListener('click', () => {
+    currentIndex++;
+    if (currentIndex >= team.length) {
+      currentIndex = 0;
+    }
+    
+    console.log('Завантажую картинку:', currentPhoto.img);
+    firstImg();
+  });
+
+  console.log('Завантажую картинку:', currentPhoto.img);
+  firstImg();
 });
