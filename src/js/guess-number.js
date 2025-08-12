3d8414ad8730ff9cf02443c8ca22e6fbbbe62e14
OlegMusu <olegmusulevskyi@gmail.com>
2025-08-12 19:36:27 +0300

    Змінення

--- src/js/guess-number.js
+++ src/js/guess-number.js
@@ -1,25 +1,31 @@
 let secretNumber = Math.floor(Math.random() * 10);
 
-  const input = document.querySelector('input[type="number"]');
-  const button = document.querySelector('[data-btnNumber]');
-  const message = document.getElementById('result-guess-num');
-  const span = document.getElementById('span-result');
+const input = document.querySelector('input[type="number"]');
+const button = document.querySelector('[data-btnNumber]');
+const message = document.getElementById('result-guess-num');
+const span = document.getElementById('span-result');
 
-  message.style.display = 'none';
+message.style.display = 'none';
 
-  button.addEventListener('click', () => {
-    const userGuess = Number(input.value);
+button.addEventListener('click', () => {
+  const userGuess = Number(input.value);
 
-    if (isNaN(userGuess) || userGuess < 0 || userGuess > 9) {
-      alert('Введіть число від 0 до 9');
-      return;
-    }
+  if (isNaN(userGuess) || userGuess < 0 || userGuess > 9) {
+    message.style.display = 'block';
+    message.style.color = 'red';
+    message.textContent = 'Введіть число від 0 до 9';
+    return;
+  }
 
-    if (userGuess === secretNumber) {
-      span.textContent = secretNumber;
-      message.style.display = 'block';
-      secretNumber = Math.floor(Math.random() * 10);
-    } else {
-      alert('Ви програли, спробуйте ще раз.');
-    }
-  });
+message.style.display = 'block';
+
+  if (userGuess === secretNumber) {
+    span.textContent = secretNumber;
+    message.style.color = 'green';
+    message.innerHTML = `Вітаю, ви вгадали число! <span id="span-result">${secretNumber}</span>`;
+    secretNumber = Math.floor(Math.random() * 10);
+  } else {
+    message.style.color = 'red';
+    message.textContent = 'Ви програли, спробуйте ще раз.';
+  }
+});
