e2b4fbe9f97e3d0c4a2f197af489d24d043574a4
Nierochron <166538574+Nierochron@users.noreply.github.com>
2025-08-12 17:51:52 +0200

    add completly header + modal
    Добавив header та пару маленьких змін

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,2 +1,42 @@
-const titleEl = document.querySelector("h1")
-console.log(titleEl)
+const modal = document.getElementById("welcomeModal");
+const modalClose = document.getElementById("modalClose");
+const body = document.body;
+const html = document.documentElement;
+
+const scrollbarWidth = window.innerWidth - html.clientWidth;
+
+function disableScroll() {
+  body.style.overflow = "hidden";
+  body.style.marginRight = scrollbarWidth + "px";
+}
+
+function enableScroll() {
+  body.style.overflow = "";
+  body.style.marginRight = "";
+}
+
+function openModal() {
+  modal.style.display = "flex";
+  disableScroll();
+}
+
+function closeModal() {
+  modal.style.display = "none";
+  enableScroll();
+}
+
+modalClose.addEventListener("click", closeModal);
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+window.addEventListener("load", () => {
+  openModal();
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && modal.style.display === "flex") {
+    closeModal();
+  }
+});
