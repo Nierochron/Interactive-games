5b2ed3ff0261a847d1460058d64963a533a1b227
OlegMusu <olegmusulevskyi@gmail.com>
2025-08-13 21:47:52 +0300

    Виправлення

--- src/js/guess-number.js
+++ src/js/guess-number.js
@@ -1,31 +1,25 @@
 let secretNumber = Math.floor(Math.random() * 10);
 
-const input = document.querySelector('input[type="number"]');
+const input = document.querySelector('#guess-number');
 const button = document.querySelector('[data-btnNumber]');
 const message = document.getElementById('result-guess-num');
-const span = document.getElementById('span-result');
-
-message.style.display = 'none';
 
 button.addEventListener('click', () => {
   const userGuess = Number(input.value);
+    console.log("userGuess", userGuess)
 
   if (isNaN(userGuess) || userGuess < 0 || userGuess > 9) {
-    message.style.display = 'block';
     message.style.color = 'red';
     message.textContent = 'Введіть число від 0 до 9';
     return;
   }
 
-message.style.display = 'block';
-
   if (userGuess === secretNumber) {
     span.textContent = secretNumber;
     message.style.color = 'green';
     message.innerHTML = `Вітаю, ви вгадали число! <span id="span-result">${secretNumber}</span>`;
-    secretNumber = Math.floor(Math.random() * 10);
   } else {
     message.style.color = 'red';
-    message.textContent = 'Ви програли, спробуйте ще раз.';
+    message.innerHTML = `Ви програли, спробуйте ще раз! Компютер загадав <span id="span-result">${secretNumber}</span>`;
   }
 });
